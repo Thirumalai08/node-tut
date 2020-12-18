95e78c5c3a7ddec9a5f0f928e700169f6a59090b
Thirumalai08 <thirunambi422@gmail.com>
2020-12-18 14:57:31 +0530

    CR Function for name.js

--- name.js
+++ name.js
@@ -1,27 +1,24 @@
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 5000
 // JSON Data
 let data = [
-    { id: 1, title: 'Create a project',  createdOn: new Date() },
-    { id: 2, title: 'Take a cofféé',     createdOn: new Date() },
-    { id: 3, title: 'Write new article', createdOn: new Date() },
-    { id: 4, title: 'Walk toward home',  createdOn: new Date() },
-    { id: 5, title: 'Have some dinner',  createdOn: new Date() },
+    { id: 1, name:'Test name',  createdOn: new Date() },
+    { id: 2, name:'Test name by id',  createdOn: new Date() },
 ];
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    const output = { value:  'hello world!' }
+    const output = 'Rest API Welcomes You To see the values route to /getNames'
     res.send(output)
 })
 // Get All users
-app.get('/users',(req,res)=>{
+app.get('/getNames',(req,res)=>{
     res.status(200).json(data);
 })
 // Get Users by ID
-app.get('/users/:id',(req,res)=>{
+app.get('/getNames/:id',(req,res)=>{
     // find the data object with respect to id
     let found = data.find(function (item) {
         return item.id === parseInt(req.params.id);
@@ -34,14 +31,14 @@ app.get('/users/:id',(req,res)=>{
     }
 })
 // Post
-app.post('/users/add',(req,res)=>{
+app.post('/addName',(req,res)=>{
     let userId = data.map(user=>user.id)
     // newId for new User
     let newUserId = userId.length > 0 ? Math.max.apply(Math,userId) + 1 : 1
     // Creating new user
     let newUser = {
         id: newUserId,
-        title: req.body.title,
+        name: req.body.name,
         created: new Date()
     }
     // pushing into JSON Data array
@@ -49,6 +46,6 @@ app.post('/users/add',(req,res)=>{
     res.status(201).json(newUser)
 })
 //configure the server port
-app.listen(3000, () => {
-    console.log('Server runs on port 3000')
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
 })
